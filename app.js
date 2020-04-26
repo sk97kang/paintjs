443e8ea850e11ac3c14b071b799a3fe839806080
Kan <sk97.kang@gmail.com>
2020-04-26 13:29:32 +0900

    #2.4 Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,19 +11,20 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
-function startPainting(){
+function startPainting() {
     painting = true;
 }
 
-function stopPainting(){
+function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event){
+function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
-    if(!painting){
+    if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
 
@@ -31,14 +34,14 @@ function onMouseMove(event){
     }
 }
 
-if(canvas){
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting)
 }
 
-function handleColorClick(event){
+function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
 }
@@ -46,3 +49,27 @@ function handleColorClick(event){
 Array.from(colors).forEach(color => {
     color.addEventListener("click", handleColorClick);
 });
+
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
